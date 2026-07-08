c9752dca3777ae25da9c43943df911eec6e4c82b
ayali-copilot <ayaly03052004@gmail.com>
2026-07-08 12:30:56 +0300

    try

--- src/constants/chartConstants.js
+++
@@ -1,9 +0,0 @@
-export const BUDGET_COLORS = {
-  HR: '#1e3a5f',
-  PROC: '#2563eb',
-  PLANNED: '#93c5fd',
-};
-
-export const INITIAL_VISIBLE_PROJECTS_COUNT = 4;
-
-export default { BUDGET_COLORS, INITIAL_VISIBLE_PROJECTS_COUNT };
